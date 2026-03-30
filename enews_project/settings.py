5dd0eee3c1f6bb4d66d74a3bf8afbf1196dad42b
anwinanandisaac <156329897+anwinanandisaac@users.noreply.github.com>
2026-03-30 11:47:25 +0530

    inital commit

--- enews_project/settings.py
+++ enews_project/settings.py
@@ -101,10 +101,18 @@ DATABASES = {
     }
 }
 '''
+
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
