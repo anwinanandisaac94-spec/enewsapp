ac9dba265ef6e6341bca600e802f409b5aad2f83
anwinanandisaac <156329897+anwinanandisaac@users.noreply.github.com>
2026-03-30 10:54:49 +0530

    first commit

---
+++ enews/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import User , Article
+from django.contrib.auth.decorators import login_required
+
+def welcome(request):
+    return render(request, 'home.html')
+
+def login_options(request):
+    return render(request, 'welcome.html')
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, username=email, password=password)
+        if user:
+            if user.role != 'admin':
+                login(request, user)  #session creation
+                return redirect('user_home') # user home
+            elif user.role == 'admin':
+                login(request, user)
+                return redirect('admin_home') # admin home 
+            else:
+                return render(request, "login.html", {"error": "Invalid credentials"})
+        else:
+           return render(request, "login.html", {"error": "Invalid credentials"})
+    return render(request, "login.html")
+
+
+
+def register(request):
+    if request.method == "POST":
+        fullname = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        username=email
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match")
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect('register')
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            first_name=fullname
+        )
+
+        user.save()
+        messages.success(request, "Account created successfully")
+        return redirect('login')
+
+    return render(request, 'register.html')
+
+@login_required
+def user_home(request):
+    articles = Article.objects.all().order_by('-created_at')
+    return render(request, 'user_home.html',{ 'articles': articles})
+
+
+def admin_home(request):
+    return render(request, 'dashboard.html')
+
+def editor_home(request):
+    return render(request, 'editor_home.html')
+
+def reporter_home(request):
+    return render(request, 'reporter_home.html')
+
+
+def user_logout(request):
+    logout(request)  # destroys session
+    return redirect('login')  # redirect to login page
+
+def admin_article(request):
+    if request.method == "POST":
+        image = request.FILES.get('image')
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Article.objects.create(
+            title=title,
+            description=description,
+            image=image,
+            author=request.user
+        )
+        return render(request, 'article_creation.html',{'message':'Article Saved'})
+    return render(request, 'article_creation.html')
